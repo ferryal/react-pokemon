b146433056f2c98a6b454403502c3eca74393ae0
ferryal <ferryal.bahari@amartha.com>
2021-02-27 18:36:46 +0700

    feat: add query get detail pokemon

--- src/GraphQL/Queries.js
+++ src/GraphQL/Queries.js
@@ -17,6 +17,29 @@ const GET_LIST_POKEMON = gql`
     }
 `;
 
+const GET_DETAIL_POKEMON = gql`
+  query pokemon($name: String!) {
+    pokemon(name: $name) {
+      id
+      name
+      sprites {
+        front_default
+      }
+      moves {
+        move {
+          name
+        }
+      }
+      types {
+        type {
+          name
+        }
+      }
+    }
+  }
+`;
+
 export {
   GET_LIST_POKEMON,
+  GET_DETAIL_POKEMON,
 };
